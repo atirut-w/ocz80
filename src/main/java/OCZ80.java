f28e10e32a4e6624f9656ca9bb3e90fbb2b9eeea
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-11-25 12:35:22 +0700

    Stubs

---
+++ src/main/java/OCZ80.java
@@ -0,0 +1,9 @@
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = OCZ80.MODID, name = OCZ80.NAME, version = OCZ80.VERSION)
+public class OCZ80
+{
+    public static final String MODID = "ocz80";
+    public static final String NAME = "OCZ80";
+    public static final String VERSION = "1.12.2-0.1.0";
+}
