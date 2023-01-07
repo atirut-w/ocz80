26142ccb66100751cbd0bac3a69b354ad17632da
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-01-07 22:28:14 +0700

    Improve EEPROM loading

--- src/main/java/com/github/atirut/ocz80/state/Run.java
+++ src/main/java/com/github/atirut/ocz80/state/Run.java
@@ -3,16 +3,18 @@ package com.github.atirut.ocz80.state;
 import com.github.atirut.ocz80.Arch;
 import com.github.atirut.ocz80.OCZ80;
 
-import li.cil.oc.api.machine.ExecutionResult;
 import li.cil.oc.api.machine.Machine;
 
 public class Run extends State {
-    private static ExecutionResult SYNCHRONOUS_CALL = new ExecutionResult.SynchronizedCall();
+    private static int KIBIBYTE = 1024;
+    private static int PAGESIZE = 4 * KIBIBYTE;
+
     private byte[] eeprom;
 
     public Run(Arch arch, Machine machine, byte[] eeprom) {
         super(arch, machine);
-        this.eeprom = eeprom;
+        this.eeprom = new byte[PAGESIZE];
+        System.arraycopy(eeprom, 0, this.eeprom, 0, Math.min(this.eeprom.length, eeprom.length));
     }
 
     public boolean isInitialized() {
