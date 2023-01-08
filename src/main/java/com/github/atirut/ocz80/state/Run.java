9c33a088249caaf95cd8f01b6ff28e82d504e19c
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-01-08 11:49:59 +0700

    Progress on CPU reimplementation

--- src/main/java/com/github/atirut/ocz80/state/Run.java
+++ src/main/java/com/github/atirut/ocz80/state/Run.java
@@ -13,7 +13,13 @@ public class Run extends State {
     private byte[][] ram;
     private byte[] mmap;
 
+    private byte[] main = new byte[8];
+    private byte[] alternate = new byte[8];
+
     private short pc;
+    private short sp;
+    private byte flags;
+    private boolean running = true;
 
     public Run(Arch arch, Machine machine, byte[] eeprom) {
         super(arch, machine);
@@ -28,7 +34,31 @@ public class Run extends State {
     }
 
     public Transition runThreaded() {
-        OCZ80.logger.info(String.format("$%04x: $%02x", pc, read(pc++)));
+        if (!running) {
+            return new Transition(this, SLEEP_ZERO);
+        }
+
+        byte opcode = fetch();
+        switch (opcode) {
+            default: // Unprefixed opcodes
+                Instruction op = new Instruction(opcode);
+                switch (op.x) {
+                    case 0:
+                        switch (op.z) {
+                            case 6:
+                                main[op.y] = fetch();
+                        }
+                    case 1:
+                        if (op.z == 6 && op.y == 6) {
+                            running = false;
+                            OCZ80.logger.info(String.format("CPU halted at $%04x", (int)(pc - 1)));
+                            OCZ80.logger.info(String.format("AF = $%04x", (int)readRegisterPair(3, true)));
+                            OCZ80.logger.info(String.format("BC = $%04x", (int)readRegisterPair(0, true)));
+                            OCZ80.logger.info(String.format("DE = $%04x", (int)readRegisterPair(1, true)));
+                            OCZ80.logger.info(String.format("HL = $%04x", (int)readRegisterPair(2, true)));
+                        }
+                }
+        }
 
         return new Transition(this, SLEEP_ZERO);
     }
@@ -37,12 +67,41 @@ public class Run extends State {
 
     }
 
+    private short readRegisterPair(int pair, boolean af) {
+        if (pair < 3) {
+            return (short)((main[pair * 2] << 8) | main[(pair * 2) + 1]);
+        } else {
+            if (!af) {
+                return sp;
+            } else {
+                return (short)((main[7] << 8) | flags);
+            }
+        }
+    }
+
+    private byte fetch() {
+        return read(pc++);
+    }
+
     private byte read(short address) {
         if (mmap[address >> 12] == 0) {
             return eeprom[address & 0xfff];
         }
         else {
             return ram[mmap[address >> 12] - 1][address & 0xfff];
+        }
+    }
+
+    private class Instruction {
+        final byte x, y, z, p, q;
+
+        public Instruction(byte opcode) {
+            x = (byte)((opcode >> 6) & 0x03);
+            y = (byte)((opcode >> 3) & 0x07);
+            z = (byte)(opcode & 0x07);
+
+            p = (byte)(y >> 1);
+            q = (byte)(y % 2);
         }
     }
 }
